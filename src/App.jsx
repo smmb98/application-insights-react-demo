51bacb3ffe8f3adad024d6956b815fb6d2dad54f
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-05-24 13:27:37 +0500

    added essential files

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react';
-import { AppInsightsContext } from '@microsoft/applicationinsights-react-js';
-import { reactPlugin } from './ApplicationInsightsService';
-import TestComponent from './TestComponent';
-import './App.css';
+import { useEffect } from "react";
+import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
+import { reactPlugin } from "./ApplicationInsightsService";
+import TestComponent from "./TestComponent";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    (async function () {
+      const { text } = await (await fetch(`/api/message`)).json();
+    })();
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Home
-        </p>
+        <p>Home</p>
       </header>
       <AppInsightsContext.Provider value={reactPlugin}>
-        <TestComponent/>
+        <TestComponent />
       </AppInsightsContext.Provider>
     </div>
   );
 }
 
 export default App;
-
