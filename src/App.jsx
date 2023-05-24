9162549d59293226c06e42f82cb3676b76ce7673
S.M. Mohibullah Bukhari <mohibullahbukhari98@gmail.com>
2023-05-24 11:03:52 +0500

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -13,78 +13,44 @@ import {
 function App() {
   const [count, setCount] = useState(0);
   const [data, setData] = useState("");
-  let appInsights = getAppInsights();
   // let appInsights = getAppInsights();
+  let appInsights = null;
   // const appInsights = new ApplicationInsights({
   //   config: {
   //     instrumentationKey: "8b4f377e-de4c-4689-8bd1-7a1895cc72a3",
   //   },
   // });
   // appInsights.loadAppInsights();
-  function trackException() {
-    // appInsights.trackException({
-    //   error: new Error("some error"),
-    //   severityLevel: SeverityLevel.Error,
-    // });
-
-    // appInsights.trackException({
-    //   exception: new Error("some error"),
-    //   // severityLevel: SeverityLevel.Error,
-    // });
-
-    try {
-      console.log("trackException");
-      throw new Error("This is a simulated error.");
-    } catch (error) {
-      // Log the error
-      appInsights.trackException({ exception: error });
-    }
+  function trackExceptionHandler() {
+    console.log("trackException");
+    appInsights.trackException({
+      exception: new Error("Simulated error from react app"),
+      severityLevel: SeverityLevel.Error,
+    });
   }
 
-  function trackTrace() {
+  function trackTraceHandler() {
     console.log("trackTrace");
     appInsights.trackTrace({
-      message: "some trace",
-      // severityLevel: SeverityLevel.Information,
+      message: "Simulated trace from react app",
+      severityLevel: SeverityLevel.Information,
     });
-    // appInsights.trackTrace({
-    //   message: "some trace",
-    //   severityLevel: SeverityLevel.Information,
-    // });
   }
 
-  function trackEvent() {
+  function trackEventHandler() {
     console.log("trackEvent");
-    appInsights.trackEvent({ name: "some event" });
+    appInsights.trackEvent({ name: "Simulated event from react app" });
   }
 
-  // function throwError() {
-  //   try {
-  //     throw new Error("This is a simulated error.");
-  //   } catch (error) {
-  //     // Log the error
-  //     appInsights.trackException({ exception: error });
-  //   }
+  function ajaxRequestHandler() {
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://httpbin.org/status/200");
+    xhr.send();
+  }
 
-  //   // console.log("throwError");
-  //   // throw Error("some error");
-  //   let foo = {
-  //     field: { bar: "value" },
-  //   };
-
-  //   // This will crash the app; the error will show up in the Azure Portal
-  //   return foo.field.bar;
-  // }
-
-  // function ajaxRequest() {
-  //   let xhr = new XMLHttpRequest();
-  //   xhr.open("GET", "https://httpbin.org/status/200");
-  //   xhr.send();
-  // }
-
-  // function fetchRequest() {
-  //   fetch("https://httpbin.org/status/200");
-  // }
+  function fetchRequestHandler() {
+    fetch("https://httpbin.org/status/200");
+  }
 
   useEffect(() => {
     (async function () {
@@ -98,6 +64,9 @@ function App() {
       instrumentationKey="8b4f377e-de4c-4689-8bd1-7a1895cc72a3"
       after={() => {
         appInsights = getAppInsights();
+        if (appInsights) {
+          console.log("appInsights is now initialized");
+        }
       }}
     >
       <div>
@@ -117,14 +86,16 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <button onClick={trackException}>Track Exception</button>
-      <button onClick={trackEvent}>Track Event</button>
-      <button onClick={trackTrace}>Track Trace</button>
+      <button onClick={trackExceptionHandler}>Track Exception</button>
+      <button onClick={trackEventHandler}>Track Event</button>
+      <button onClick={trackTraceHandler}>Track Trace</button>
       {/* <button onClick={throwError}>Autocollect an Error</button> */}
-      {/* <button onClick={ajaxRequest}>
+      <button onClick={ajaxRequestHandler}>
         Autocollect a Dependency (XMLHttpRequest)
       </button>
-      <button onClick={fetchRequest}>Autocollect a dependency (Fetch)</button> */}
+      <button onClick={fetchRequestHandler}>
+        Autocollect a dependency (Fetch)
+      </button>
       {/* <div>{data}</div> */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter, Link, Route } from "react-router-dom";
-import TelemetryProvider from "./telemetry-provider";
-import { getAppInsights } from "./TelemetryService";
-import { SeverityLevel } from "@microsoft/applicationinsights-web";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
